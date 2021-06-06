02702c776e937279b3d9f71da28d0f8a092c92ab
RobinHanxy <717740827@qq.com>
2021-06-06 20:48:45 +0800

    update figure

---
+++ fMRI_statistics/fMRI_twosampleT_mALFF_job.m
@@ -0,0 +1,146 @@
+%-----------------------------------------------------------------------
+% Job saved on 06-Jun-2021 19:03:36 by cfg_util (rev $Rev: 7345 $)
+% spm SPM - SPM12 (7487)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.stats.factorial_design.dir = {'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\res'};
+%%
+matlabbatch{1}.spm.stats.factorial_design.des.t2.scans1 = {
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\capf\sub1506_capf_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\capf\sub1509_capf_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\capf\sub1511_capf_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\capf\sub1703_capf_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\capf\sub1706_capf_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\capf\sub1707_capf_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\capf\sub1802_capf_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\capf\sub1803_capf_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\capf\sub2002_capf_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\capf\sub2211_capf_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\capf\sub2401_capf_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\capf\sub2512_capf_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\capf\sub2513_capf_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\capf\sub2515_capf_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\capf\sub2516_capf_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\capm\sub1502_capm_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\capm\sub1507_capm_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\capm\sub1808_capm_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\capm\sub1809_capm_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\capm\sub1810_capm_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\capm\sub2008_capm_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\capm\sub2009_capm_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\capm\sub2011_capm_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\capm\sub2013_capm_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\capm\sub2015_capm_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\capm\sub2212_capm_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\capm\sub2213_capm_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\capm\sub2214_capm_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\capm\sub2514_capm_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\capm\sub2617_capm_mALFFMap.nii,1'
+                                                           };
+%%
+%%
+matlabbatch{1}.spm.stats.factorial_design.des.t2.scans2 = {
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\faf\sub0103_faf_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\faf\sub0106_faf_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\faf\sub0109_faf_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\faf\sub0302_faf_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\faf\sub0604_faf_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\faf\sub0709_faf_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\faf\sub0808_faf_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\faf\sub1101_faf_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\faf\sub1102_faf_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\faf\sub1103_faf_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\faf\sub1206_faf_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\faf\sub1211_faf_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\faf\sub1214_faf_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\faf\sub1403_faf_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\fam\sub0312_fam_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\fam\sub0313_fam_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\fam\sub0509_fam_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\fam\sub0511_fam_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\fam\sub0601_fam_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\fam\sub0602_fam_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\fam\sub0801_fam_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\fam\sub0802_fam_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\fam\sub0804_fam_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\fam\sub1207_fam_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\fam\sub1208_fam_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\fam\sub1209_fam_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\fam\sub1212_fam_mALFFMap.nii,1'
+                                                           'E:\FDU\ISTBI\fMRI\pm2.5-offspring-mouse\ALFF\fam\sub1414_fam_mALFFMap.nii,1'
+                                                           };
+%%
+matlabbatch{1}.spm.stats.factorial_design.des.t2.dept = 0;
+matlabbatch{1}.spm.stats.factorial_design.des.t2.variance = 1;
+matlabbatch{1}.spm.stats.factorial_design.des.t2.gmsca = 0;
+matlabbatch{1}.spm.stats.factorial_design.des.t2.ancova = 0;
+%%
+matlabbatch{1}.spm.stats.factorial_design.cov.c = [1
+                                                   1
+                                                   1
+                                                   1
+                                                   1
+                                                   1
+                                                   1
+                                                   1
+                                                   1
+                                                   1
+                                                   1
+                                                   1
+                                                   1
+                                                   1
+                                                   1
+                                                   0
+                                                   0
+                                                   0
+                                                   0
+                                                   0
+                                                   0
+                                                   0
+                                                   0
+                                                   0
+                                                   0
+                                                   0
+                                                   0
+                                                   0
+                                                   0
+                                                   0
+                                                   1
+                                                   1
+                                                   1
+                                                   1
+                                                   1
+                                                   1
+                                                   1
+                                                   1
+                                                   1
+                                                   1
+                                                   1
+                                                   1
+                                                   1
+                                                   1
+                                                   0
+                                                   0
+                                                   0
+                                                   0
+                                                   0
+                                                   0
+                                                   0
+                                                   0
+                                                   0
+                                                   0
+                                                   0
+                                                   0
+                                                   0
+                                                   0];
+%%
+matlabbatch{1}.spm.stats.factorial_design.cov.cname = 'gender';
+matlabbatch{1}.spm.stats.factorial_design.cov.iCFI = 1;
+matlabbatch{1}.spm.stats.factorial_design.cov.iCC = 1;
+matlabbatch{1}.spm.stats.factorial_design.multi_cov = struct('files', {}, 'iCFI', {}, 'iCC', {});
+matlabbatch{1}.spm.stats.factorial_design.masking.tm.tm_none = 1;
+matlabbatch{1}.spm.stats.factorial_design.masking.im = 1;
+matlabbatch{1}.spm.stats.factorial_design.masking.em = {'E:\FDU\ISTBI\fMRI\fd_template_mouse\Reslice_3mm_TMBTA_Brain_Mask.nii,1'};
+matlabbatch{1}.spm.stats.factorial_design.globalc.g_omit = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.gmsca.gmsca_no = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.glonorm = 1;
